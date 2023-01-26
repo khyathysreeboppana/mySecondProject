ca1dadd0155f60456d49a2e916e1f1f67cd6fa02
khyathysreeboppana <khyathy.boppana@copart.com>
2023-01-26 16:13:53 -0500

    test

---
+++ demoTest.java
@@ -0,0 +1,11 @@
+public class demoTest {
+ public static void main(String[] args){
+ System.out.println("java project");
+ System.out.println("java project1");
+ int x = 5;
+ int y = 6;
+ int z = x+y;
+System.out.println("sum of x and y " +z);
+        }
+
+}
